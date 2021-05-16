7b7ba4271d9ced1c6191a45911ab64ad8ebcdb42
DalatCoder <hieuntctk42@gmail.com>
2021-05-16 17:47:40 +0700

    Optimize CSS 🐱‍🏍

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    enabled: false,
-    content: ['./public/**/*.html']
+    enabled: true,
+    content: ['./public/**/*.html', '*.html']
   },
   theme: {
     debugScreens: {
